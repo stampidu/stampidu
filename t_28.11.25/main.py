b82370862eb3089e157a42754884c41c8808c905
stampidu <86229569+stampidu@users.noreply.github.com>
2025-11-28 23:05:02 +0300

    proj_29.11.25

---
+++ t_28.11.25/main.py
@@ -0,0 +1,15 @@
+
+a = [3, 5, 7]
+b = 5
+
+if b in a:
+    print('Верно')
+
+else:
+    print('Повторите вод:')
+
+########################
+a = "Privet moi dorogie. Davaite znakomitsa."
+
+ind2 = a.find("moi")
+print(f"pervoe vhogdenie: {ind2}")
